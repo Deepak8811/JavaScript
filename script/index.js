6f4b968315659fcfb1caf6f3a1a659c7a24166e8
Deepak chaurasiya <deepchaurasiya1997@gmail.com>
2024-05-23 12:58:19 +0530

    Proomise.allSettled()

--- script/index.js
+++ script/index.js
@@ -3083,21 +3083,29 @@ const p1 = new Promise((resolve, reject) => {
 
 const p2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("P2 Success");
-    // reject("P2 Failed");
+    // resolve("P2 Success");
+    reject("P2 Failed");
   }, 1000)
 })
 const p3 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    // resolve("P3 Success");
-    reject("P3 Failed");
+    resolve("P3 Success");
+    // reject("P3 Failed");
   }, 2000)
 })
 
 
 
-Promise.all([p1, p2, p3]).then((res) => {
-  console.log(res)
-}).catch((error) => {
-  console.error(error);
-})
+// Promise.all([p1, p2, p3]).then((res) => {
+//   console.log(res)
+// }).catch((error) => {
+//   console.error(error);
+// })
+
+Promise.allSettled([p1, p2, p3])
+  .then((res) => {
+    console.log(res)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
