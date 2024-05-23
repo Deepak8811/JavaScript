777f55329da4a446a2bf196fd657be89a365f1be
Deepak chaurasiya <deepchaurasiya1997@gmail.com>
2024-05-23 13:06:01 +0530

    Promise.any([])

--- script/index.js
+++ script/index.js
@@ -3077,23 +3077,24 @@ This code defines an object named "baby" with a property "name" set to "senorita
 
 const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("P1 Success");
+    // resolve("P1 Success");
+    reject("Pi Failed")
   }, 3000)
 })
 
 
 const p2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("P2 Success");
-    // reject("P2 Failed");
+    // resolve("P2 Success");
+    reject("P2 Failed");
   }, 1000)
 })
 
 
 const p3 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("P3 Success");
-    // reject("P3 Failed");
+    // resolve("P3 Success");
+    reject("P3 Failed");
   }, 2000)
 })
 
@@ -3119,11 +3120,20 @@ const p3 = new Promise((resolve, reject) => {
 
 
 
-Promise.race([p1, p2, p3])
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.error(error);
-  })
+// Promise.race([p1, p2, p3])
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   })
 
+
+
+Promise.any([p1, p2, p3])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err)
+  })
